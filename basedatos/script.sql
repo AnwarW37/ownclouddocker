136a5df466e3fe51177c1e15d8ca9c24da361e08
AnwarW37 <awarraka01@iesalbarregas.es>
2024-12-16 22:28:37 +0100

    Delete basedatos directory

--- basedatos/script.sql
+++
@@ -1,8 +0,0 @@
--- Crear el usuario
-CREATE USER 'owncloud_user'@'192.168.20.13' IDENTIFIED BY 'owncloud_password';
-
--- Otorgar privilegios sobre la base de datos "owncloud"
-GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud_user'@'192.168.20.13';
-
--- Recargar los privilegios
-FLUSH PRIVILEGES;

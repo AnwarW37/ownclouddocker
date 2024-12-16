302360c3f4866d4439453cefa14c65b61cd42221
AnwarW37 <awarraka01@iesalbarregas.es>
2024-12-16 22:51:26 +0100

    Update script.sql

--- basedatos/script.sql
+++ basedatos/script.sql
@@ -1,8 +1,3 @@
--- Crear el usuario
-CREATE USER 'owncloud_user'@'192.168.20.13' IDENTIFIED BY 'owncloud_password';
-
--- Otorgar privilegios sobre la base de datos "owncloud"
-GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud_user'@'192.168.20.13';
-
--- Recargar los privilegios
+CREATE USER 'owncloud_user'@'192.168.20.%' IDENTIFIED BY 'owncloud_password';
+GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud_user'@'192.168.20.%';
 FLUSH PRIVILEGES;
